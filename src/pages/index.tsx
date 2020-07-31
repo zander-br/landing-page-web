acd81727e8ca7d5e78d4c5f5e10e54b5ae12e38f
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-07-31 19:08:24 -0300

    feat: add first types and logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,11 @@ import JsonSchema from 'components/JsonSchema'
 
 import client from 'graphql/client'
 import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
+import { LandingPageProps } from 'types/api'
 
-const Index = () => (
+const Index = ({ logo }: LandingPageProps) => (
   <>
-    <SectionHero />
+    <SectionHero logo={logo} />
     <SectionAboutProject />
     <SectionTech />
     <SectionConcepts />
